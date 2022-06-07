70d9b01c9528dc4b8950d033b1b98adaed4f94a3
jiangjunqing <jiangjunqing@linklogis.com>
2022-06-07 17:11:28 +0800

    dijkstra算法3

---
+++ src/com/jjq/ap/dijkstra/Dijkstra.java
@@ -0,0 +1,41 @@
+package com.jjq.ap.dijkstra;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author jiangjunqing
+ * @date 2022/6/6
+ * @description: Dijkstra算法
+ * @version:
+ */
+public class Dijkstra {
+
+
+    /**
+     * 执行路口
+     * @param args
+     */
+    public static void main(String[] args) {
+        RoadGraph roadGraph = new RoadGraph(9, 9);
+        roadGraph.buildGraph();
+        int[] route = calcShortestRoute(0, roadGraph);
+        System.out.println("最短路径是:" + Arrays.toString(route));
+    }
+
+    /**
+     * 计算最短路径
+     * @param target
+     * @param roadGraph
+     * @return
+     */
+    public static int[] calcShortestRoute(int target, RoadGraph roadGraph) {
+
+        Integer[] excludeNodes = new Integer[roadGraph.getNodeSize()];
+        int tmpTarget = target;
+        int[] connects = roadGraph.getConnects(tmpTarget, excludeNodes);
+
+        return null;
+    }
+}
--- src/com/jjq/ap/dijkstra/RoadGraph.java
+++ src/com/jjq/ap/dijkstra/RoadGraph.java
@@ -1,12 +1,15 @@
 package com.jjq.ap.dijkstra;
 
 
+import java.util.Arrays;
 import java.util.List;
 
+/**
+ * 路径图表达,使用二维数据,另外提供一些工具方法,column列表示各个节点,row代表能直接连通的节点,值代表了距离
+ */
 public class RoadGraph {
 
     private int[][] data;
-
 
 
     public RoadGraph() {
@@ -27,18 +30,44 @@ public class RoadGraph {
         }
         this.data[0][1] = 4;
         this.data[0][7] = 8;
+
+        this.data[1][0] = 4;
         this.data[1][2] = 8;
         this.data[1][7] = 11;
+
+        this.data[2][1] = 8;
         this.data[2][3] = 7;
         this.data[2][5] = 4;
         this.data[2][8] = 2;
+
+        this.data[3][2] = 7;
         this.data[3][4] = 9;
         this.data[3][5] = 14;
+
+        this.data[4][3] = 9;
         this.data[4][5] = 10;
-        this.data[5][6] = 4;
+
+        this.data[5][2] = 4;
+        this.data[5][3] = 14;
+        this.data[5][4] = 10;
+        this.data[5][6] = 2;
+
+        this.data[6][5] = 2;
         this.data[6][7] = 1;
         this.data[6][8] = 6;
+
+        this.data[7][0] = 8;
+        this.data[7][1] = 11;
+        this.data[7][6] = 1;
         this.data[7][8] = 7;
+
+        this.data[8][2] = 2;
+        this.data[8][6] = 6;
+        this.data[8][7] = 7;
+    }
+
+    public int getNodeSize() {
+        return this.data[0].length;
     }
 
     /**
@@ -53,29 +82,23 @@ public class RoadGraph {
 
     /**
      * 获取相连的节点
-     * @param target
+     * @param column
      * @param excludes 排查的节点
      * @return
      */
-    public int[] getConnects(int target, List<Integer> excludes) {
-
-
-
-        return null;
+    public int[] getConnects(int column, Integer... excludes) {
+        List<Integer> excludeList = Arrays.asList(excludes);
+        //获取行数
+        int rowNum = this.data[column].length;
+        int[] resultTmp = new int[rowNum];
+        //遍历行数,取this.data[column][i]的值大于0,并返回所有符合要求i
+        for (int i = 0; i < rowNum; i++) {
+            if (this.data[column][i] > 0 && !excludeList.contains(i)) {
+                resultTmp[i] = i;
+            } else {
+                resultTmp[i] = -1;
+            }
+        }
+        return resultTmp;
     }
-
-    public static void main(String[] args) {
-        RoadGraph roadGraph = new RoadGraph();
-
-        roadGraph.buildGraph();
-        //第一列的值
-        int[] datum = roadGraph.data[1];
-
-
-    }
-
-
-
-
-
 }
